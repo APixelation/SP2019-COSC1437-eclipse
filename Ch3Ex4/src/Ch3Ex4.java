66d9aef1e6d1d1c450a0b9a28a0b30b2260e151a
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-02-07 21:30:27 +0900

    Completed Exercise 4 of Chapter 3

--- Ch3Ex4/src/Ch3Ex4.java
+++ Ch3Ex4/src/Ch3Ex4.java
@@ -6,7 +6,26 @@ public class Ch3Ex4
 	
 	public static void main(String[] args)
 	{
-		System.out.println("hello");
+		double cost;
+		double area;
+		
+		double bagSize;
+		
+		System.out.print("Enter the amount of fertilizer, in pounds, in one bag: ");
+		bagSize = console.nextDouble();
+		System.out.println();
+		
+		System.out.printf("Enter the cost of the %.0f pound fertilizer bag: $", bagSize);
+		cost = console.nextDouble();
+		System.out.println();
+		
+		System.out.print("Enter the area, in square feet, that can be fertilized by one bag: ");
+		area = console.nextDouble();
+		System.out.println();
+		
+		System.out.printf("The cost of the fertilizer per pound is: $%.2f%n", cost / bagSize);
+		System.out.printf("The cost of fertilizing per square foot is: $%.2f%n",  cost / area);
+		
 	}
 
 }
