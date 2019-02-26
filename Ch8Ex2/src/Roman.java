743bf9ef425c78fc847c9824177fc67c01f2dd55
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-02-26 18:56:48 +0900

    Completed MVP

--- Ch8Ex2/src/Roman.java
+++ Ch8Ex2/src/Roman.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class Roman {
 	
-	private static String romanNumeral;
 	private static int decimalNumber;
 	private static boolean convertRoman = true;
+	private static char nextChar;
 	
+	// Constructor
 	public Roman() {
-		romanNumeral = "";
-		decimalNumber = 0;
+	}
+	
+	public static void setDecimal() {
+		Roman.decimalNumber = 0;
 	}
 	
 	public static void romanToDecimal() {
@@ -17,52 +20,61 @@ public class Roman {
 		romNum = console.next().toUpperCase();
 		for (int i = 0; i < romNum.length(); i++) {
 			char currentChar = romNum.charAt(i);
+			try {
+				nextChar = romNum.charAt(i+1);
+			}
+			catch (StringIndexOutOfBoundsException e) {
+			}
 			switch(currentChar) {
 			case 'M' :
-				decimalNumber += 1000;
+				Roman.decimalNumber += 1000;
 				break;
 			case 'D' :
-				decimalNumber += 500;
+				Roman.decimalNumber += 500;
 				break;
 			case 'C' : 
-				decimalNumber += 100;
+				Roman.decimalNumber += 100;
 				break;
 			case 'L' :
-				decimalNumber += 50;
+				Roman.decimalNumber += 50;
 				break;
 			case 'X' :
-				decimalNumber += 10;
+				Roman.decimalNumber += 10;
 				break;
 			case 'V' :
-				decimalNumber += 5;
+				Roman.decimalNumber += 5;
 				break;
 			case 'I' :
-				decimalNumber += 1;
+				if (nextChar == 'V' || nextChar == 'X')
+					Roman.decimalNumber -= 1;
+				else
+					Roman.decimalNumber += 1;
 				break;
 			default :
 				System.out.printf("%c is not a roman numeral.\n", currentChar);
-			}	
+			}
 		}
-		System.out.printf("The Roman Numeral %s is %d.\n", romNum, decimalNumber);
+		System.out.printf("The roman numeral %s is %d.\n", romNum, decimalNumber);
 	}
 	
 	public static void tryAgain() {
 		String answer = "";
-		System.out.print("Convert another roman numeral? Y/N");
+		System.out.print("Convert another roman numeral? Y/N: ");
 		answer = console.next().toUpperCase();
 		
 			if (answer.equals("Y")) {
+				setDecimal();
 				return;
 			}
 			
 			else if (answer.equals("N")) {
 				System.out.println("Quitting...");
-				convertRoman = false;
+				Roman.convertRoman = false;
 			}
 			
 			else {
 				System.out.println("Did not understand answer, quitting!");
-				convertRoman = false;
+				Roman.convertRoman = false;
 			}
 			
 			
