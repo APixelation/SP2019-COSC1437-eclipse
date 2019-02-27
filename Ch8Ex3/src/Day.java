71c6c47e19d6705d96d9f45766f63aa8fb5c35ab
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-02-27 13:33:34 +0900

    Complete rework of source code, using switch and refined OOP process.

--- Ch8Ex3/src/Day.java
+++ Ch8Ex3/src/Day.java
@@ -1,57 +1,75 @@
-import java.math.*;
-import java.util.List;
 import java.util.*;
 
 public class Day {
-	private static List<String> dayOfTheWeek;
-	private static String today;
-	private static int todayIndex;
+	String name;
+	int dayNumber;
+
+	Day(String n, int d) {
+		this.name = n;
+		this.dayNumber = d;
+	}
 	
-	public Day() {
-		dayOfTheWeek = Arrays.asList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-		today = "";
-		todayIndex = 0;
+	private static Day sun = new Day("Sunday", 1);
+	private static Day mon = new Day("Monday", 2);
+	private static Day tue = new Day("Tuesday", 3);
+	private static Day wed = new Day("Wednesday", 4);
+	private static Day thu = new Day("Thursday", 5);
+	private static Day fri = new Day("Friday", 6);
+	private static Day sat = new Day("Saturday", 7);
+	
+	void printDay() {
+		System.out.println("Today is " + this.name);
+	}
+	
+	void daySelectSwitch(int num) {
+		switch(num) {
+			case 0 : 
+				sat.returnDay();
+				break;
+			case 1 : 
+				sun.returnDay();
+				break;
+			case 2 : 
+				mon.returnDay();
+				break;
+			case 3 : 
+				tue.returnDay();
+				break;
+			case 4 :
+				wed.returnDay();
+				break;
+			case 5 :
+				thu.returnDay();
+				break;
+			case 6 : 
+				fri.returnDay();
+				break;
+			case 7 :
+				sat.returnDay();
+				break;
+			case 8 :
+				sun.returnDay();
+				break;
+			default :
+				System.out.printf("Switch error, this is num : %d.\n", num);
 		}
-	
-	public static void setDay(String day) {
-		today = day;
 	}
 	
-	public static void printDay() {
-		System.out.printf("Today is %s.", today);
+	String returnDay() {
+		return this.name;
 	}
 	
-	public static String returnDay() {
-		return today;
+	int returnPrevDay() {
+		int prevDayNum = this.dayNumber - 1;
+		return prevDayNum;
 	}
 	
-	public static String nextDay() {
-		String nextDay;
-		Day.getDayIndex();
-		try {
-			nextDay = dayOfTheWeek.get(todayIndex + 1);
-		}
-		catch(ArrayIndexOutOfBoundsException e) {
-			nextDay = dayOfTheWeek.get(0);
-		}
-		return nextDay;
+	int returnNextDay() {
+		int nextDayNum = this.dayNumber + 1;
+		return nextDayNum;
 	}
 	
-	public static String previousDay() {
-		String prevDay;
-		Day.getDayIndex();
-		prevDay = dayOfTheWeek.get(todayIndex - 1);
-		return prevDay;
-	}
-	
-	public static int getDayIndex() {
-		todayIndex = Arrays.asList(dayOfTheWeek).indexOf(today);
-		return todayIndex;
-	}
 	
 	public static void main(String[] args) {
-		Day.setDay("Sunday");
-		printDay();
 	}
 }
-
