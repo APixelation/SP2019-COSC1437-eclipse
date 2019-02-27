ab671a02437790ea3f347cc4282ad7ca71eec638
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-02-27 20:22:01 +0900

    Completed MVP. Writing testing program..

--- Ch8Ex3/src/Day.java
+++ Ch8Ex3/src/Day.java
@@ -1,83 +1,115 @@
-import java.util.*;
 
 public class Day {
-	private String name;
-	private static int dayNumber;
-
-	Day(String n, int d) {
-		this.name = n;
-		this.dayNumber = d;
+	final static int SUN = 0;
+	final static int MON = 1;
+	final static int TUE = 2;
+	final static int WED = 3;
+	final static int THU = 4;
+	final static int FRI = 5;
+	final static int SAT = 6;
+	
+	public static int today;
+	
+	private static void setDay(int userDay) {
+		today = userDay;
 	}
 	
-	private static Day sun = new Day("Sunday", 1);
-	private static Day mon = new Day("Monday", 2);
-	private static Day tue = new Day("Tuesday", 3);
-	private static Day wed = new Day("Wednesday", 4);
-	private static Day thu = new Day("Thursday", 5);
-	private static Day fri = new Day("Friday", 6);
-	private static Day sat = new Day("Saturday", 7);
-	
-
-	void printDay() {
-		System.out.printf("Today is %s.\n", this.name);
+	private static void printDay(int day) {
+		System.out.print(daySelect(day) + ".\n");
 	}
 	
-	void daySelect(int num) {
-		String day = "";
-		switch(num) {
-			case 0 : 
-				day = sat.returnDay();
-				break;
-			case 1 : 
-				day = sun.returnDay();
-				break;
-			case 2 : 
-				day = mon.returnDay();
-				break;
-			case 3 : 
-				 day = tue.returnDay();
-				break;
-			case 4 :
-				day = wed.returnDay();
-				break;
-			case 5 :
-				day = thu.returnDay();
-				break;
-			case 6 : 
-				day = fri.returnDay();
-				break;
-			case 7 :
-				day = sat.returnDay();
-				break;
-			case 8 :
-				day = sun.returnDay();
-				break;
-			default :
-				System.out.printf("Switch error, this is num : %d.\n", num);
+	private static void returnNextDay() {
+		int thisDay = today; // Used so would not overwrite user's setDate. 
+		if (thisDay == SAT)
+			System.out.println("The day after Saturday is Sunday.");
+		else {
+			System.out.printf("The day after %s is ", daySelect(today));
+			thisDay += 1;
+			printDay(thisDay);
 		}
-		System.out.println(day + ".");
 	}
 	
-	String returnDay() {
-		return this.name;
+	private static void returnPrevDay() {
+		int thisDay = today; // Used so would not overwrite user's setDate.
+		if (thisDay == SUN)
+			System.out.println("The day before Sunday is Saturday");
+		else {
+			System.out.printf("The day before %s is ", daySelect(today));
+			thisDay -= 1;
+			printDay(thisDay);
+		}
 	}
 	
-	void returnPrevDay() {
-		int prevDayNum = this.dayNumber - 1;
-		System.out.printf("The day before %s is ", this.name);
-		daySelect(prevDayNum);
+	private static void calculateDay(int numOfDays) {
+		int thisDay = today; // Used so would not overwrite user's setDate;
+		thisDay += numOfDays % 7;
+		if (numOfDays == 1 || numOfDays == -1) {
+			System.out.printf("%d day from %s is ", numOfDays, daySelect(today));
+			printDay(thisDay);
+		}
+		else {
+			System.out.printf("%d days from %s is ", numOfDays, daySelect(today));
+			printDay(thisDay);
+		}
 	}
 	
-	void returnNextDay() {
-		int nextDayNum = this.dayNumber + 1;
-		System.out.printf("The next day after %s is ", this.name);
-		daySelect(nextDayNum);
+	private static String daySelect(int dayNumber) {
+		String day = "";
+		
+		switch (dayNumber) {
+		case SUN :
+		case 7 : // Used for fixing issue if a user inputed very large numbers into the calculateDay method.
+			day = "Sunday";
+			break;
+		case MON :
+		case 8 : // Used for fixing issue if a user inputed very large numbers into the calculateDay method.
+		case -6 : // Used for fixing issue if a user inputed very large negative numbers into the calculateDay method.
+			day = "Monday";
+			break;
+		case TUE :
+		case 9 : // Used for fixing issue if a user inputed very large numbers into the calculateDay method.
+		case -5 : // Used for fixing issue if a user inputed very large negative numbers into the calculateDay method.
+			day = "Tuesday";
+			break;
+		case WED :
+		case 10: // Used for fixing issue if a user inputed very large numbers into the calculateDay method.
+		case -4 : // Used for fixing issue if a user inputed very large negative numbers into the calculateDay method.
+			day = "Wednesday";
+			break;
+		case THU :
+		case 11 : // Used for fixing issue if a user inputed very large numbers into the calculateDay method.
+		case -3 : // Used for fixing issue if a user inputed very large negative numbers into the calculateDay method.
+			day = "Thursday";
+			break;
+		case FRI :
+		case 12 : // Used for fixing issue if a user inputed very large numbers into the calculateDay method.
+		case -2 : // Used for fixing issue if a user inputed very large negative numbers into the calculateDay method.
+			day = "Friday";
+			break;
+		case SAT :
+		case -1 : // Used for fixing issue if a user inputed very large negative numbers into the calculateDay method.
+			day = "Saturday";
+			break;
+		default :
+			System.out.printf("Switch error! dayNumber = %d day = %s", dayNumber, day);
+		}
+		
+		return day;
 	}
-	
 	
 	public static void main(String[] args) {
-		sat.printDay();
-		sat.returnNextDay();
-		sat.returnPrevDay();
+		int i;
+		// Tests through the days.
+		for (i = 0; i < 7; i++) {
+			int x;
+			setDay(i);
+			printDay(i);
+			returnPrevDay();
+			returnNextDay();
+			for (x = 0; x > -365; x--)
+				calculateDay(x);
+			System.out.println();
+		}
 	}
+	
 }
