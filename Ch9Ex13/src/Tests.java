f4ca228922c7990283e573ffcbe11c24b4b155c6
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-03-19 16:02:59 +0900

    Beginning Ch9 Exercise 13

---
+++ Ch9Ex13/src/Tests.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Tests {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void setClassSize(int numOfStudents) {
+		
+	}
+	private static void setName(String firstName, String lastName) {
+	}
+}
+
