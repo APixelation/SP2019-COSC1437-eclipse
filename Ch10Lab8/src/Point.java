a6a04f6baa1fa0a120398a9377501681f27946f6
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-03-26 18:41:18 +0900

    Initial Classes created.

---
+++ Ch10Lab8/src/Point.java
@@ -0,0 +1,17 @@
+
+public class Point {
+	
+	// Instance Variables
+	double x, y;
+	
+	public Point() {
+		x = 0;
+		y = 0;
+	}
+	
+	public Point(double xVar, double yVar) {
+		x = xVar;
+		y = yVar;
+	}
+}
+
